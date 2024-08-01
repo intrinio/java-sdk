147df12c7112e551d804407032f816357429540b
ssnyder-intrinio <ssnyder@intrinio.com>
2024-07-31 21:56:05 -0500

    No release notes for this build

--- src/main/java/com/intrinio/invoker/ApiClient.java
+++ src/main/java/com/intrinio/invoker/ApiClient.java
@@ -89,7 +89,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Swagger-Codegen/6.21.1/java");
+        setUserAgent("Swagger-Codegen/6.22.0/java");
 
         // Setup authentications (key: authentication name, value: authentication).
         authentications = new HashMap<String, Authentication>();
